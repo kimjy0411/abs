1547b51817d0804f2566a639cd534f03e87c6bc8
kim jae young <kbs00717@gmail.com>
2024-12-16 02:21:46 +0900

    2nd

--- abs_utils/model_utils.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-def load_yolo_model():
-    """
-    YOLOv5 모델을 로드합니다.
-    """
-    # YOLOv5 기본 모델 로드 (COCO 데이터셋)
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # 'yolov5s'는 경량 YOLOv5 모델
-    return model
